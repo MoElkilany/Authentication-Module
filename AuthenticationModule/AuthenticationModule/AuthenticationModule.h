a50b567512fadf1146cbcb59877b80278be446c7
Mohamed ELkilany <mohamedsayedelkilany@gmail.com>
2023-10-14 12:10:35 +0300

    the Creation of Example

---
+++ AuthenticationModule/AuthenticationModule/AuthenticationModule.h
@@ -0,0 +1,18 @@
+//
+//  AuthenticationModule.h
+//  AuthenticationModule
+//
+//  Created by Elkilany on 14/10/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AuthenticationModule.
+FOUNDATION_EXPORT double AuthenticationModuleVersionNumber;
+
+//! Project version string for AuthenticationModule.
+FOUNDATION_EXPORT const unsigned char AuthenticationModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AuthenticationModule/PublicHeader.h>
+
+
---
+++ AuthenticationModule/AuthenticationModule/Login.swift
@@ -0,0 +1,30 @@
+//
+//  Login.swift
+//  AuthenticationModule
+//
+//  Created by Elkilany on 14/10/2023.
+//
+
+import Foundation
+
+public class Login {
+    
+    private init(){}
+    
+    public static func validatedPasswords()->String{
+        return "validated Passwords"
+    }
+    public static func loginAction()->String{
+        return "Login Action"
+    }
+    public static func signUpAction()->String{
+        return "SignUp Action"
+    }
+    public static func forgetPasswordAction()->String{
+        return "ForgetPassword Action"
+    }
+    public static func dontHaveAccountAction()->String{
+        return "Don't have an Account Action"
+    }
+
+}
